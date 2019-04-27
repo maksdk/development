9ab32fba8dcf52a2bdc511b3fc80a41c0a0b5340
maksdk <havryshchenko@gmail.com>
2019-04-27 14:13:22 +0300

    random color

--- utils/Utils.js
+++ utils/Utils.js
@@ -8,13 +8,8 @@ Utils.randomInt = function(min, max){
 	return Math.floor(min + Math.random() * (max - min + 1));
 };
 
-Utils.randomColor = function() {
-   var letters = '0123456789ABCDEF';
-   var color = '';
-   for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-   }
-   return color;
+Utils.randomHexColor = function() {
+   return'#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
 };
 
 Utils.radToDeg = function(rad){
