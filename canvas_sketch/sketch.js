9af932ce45bd72f68050a3ac6d52ff86360d9a35
maksdk <havryshchenko@gmail.com>
2019-06-04 08:29:28 +0300

    noice2d

--- canvas_sketch/sketch.js
+++ canvas_sketch/sketch.js
@@ -8,22 +8,25 @@ const settings = {
 };
 
 const sketch = () => {
-   const colorCount = random.rangeFloor(1, 5);
+   const colorCount = random.rangeFloor(2, 5);
    const palette = random.shuffle(random.pick(palettes))
       .splice(0, colorCount);
 
    const createGrid = () => {
       const points = [];
-      const count = 20;
+      const count = 30;
 
       for (let x = 0; x < count; x++) {
          for (let y = 0; y < count; y++) {
             const u = count <= 1 ? 0.5 : x / (count - 1);
             const v = count <= 1 ? 0.5 : y / (count - 1);
+            const radius = Math.abs(random.noise2D(u, v)) * 0.2;
+
             points.push({
                color: random.pick(palette),
-               postition:[u, v],
-               radius: Math.max(0, random.gaussian() * 0.01)
+               postition: [u, v],
+               rotation: random.noise2D(u, v),
+               radius
             });
          }
       }
@@ -31,26 +34,35 @@ const sketch = () => {
       return points;
    };
 
-   random.setSeed(512);
+   //random.setSeed(512);
 
    const points = createGrid().filter(() => random.value() > 0.5);
    const margin = 400;
 
    return ({ context, width, height }) => {
       context.fillStyle = "#fff";
-      context.fillRect(0,0,width, height);
+      context.fillRect(0, 0, width, height);
 
       points.forEach((data) => {
-         const { postition, radius, color } = data;
+         const { postition, radius, color, rotation } = data;
          const [u, v] = postition;
 
          const x = lerp(margin, width - margin, u);
          const y = lerp(margin, height - margin, v);
 
-         context.beginPath();
-         context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+         // context.beginPath();
+         // context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+         // context.fillStyle = color;
+         // context.fill();
+         // console.log('font', radius * width);
+
+         context.save();
          context.fillStyle = color;
-         context.fill();
+         context.font = `${radius * width}px "Arial"`;
+         context.translate(x, y);
+         context.rotate(rotation);
+         context.fillText('=', 0, 0);
+         context.restore();
       });
    };
 };
